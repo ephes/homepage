db93882d1b73ebf303351146c47653da28e9b204
Jochen Wersdörfer <jochen@wersdoerfer.de>
2017-09-18 15:51:55 +0200

    responsive images

--- homepage/blogs/admin.py
+++ homepage/blogs/admin.py
@@ -6,20 +6,22 @@ from .models import Image
 
 
 class BlogModelAdmin(admin.ModelAdmin):
-       list_display = ('title', 'user')
+    list_display = ('title', 'user')
 
 admin.site.register(Blog, BlogModelAdmin)
 
 
 class BlogPostModelAdmin(admin.ModelAdmin):
-       list_display = ('title', 'author', 'blog')
-       class Media:
-           js = ('js/ckeditor_fix.js',)
+    list_display = ('title', 'author', 'blog')
+
+    class Media:
+        js = ('js/ckeditor_fix.js',)
 
 admin.site.register(BlogPost, BlogPostModelAdmin)
 
 
 class ImageModelAdmin(admin.ModelAdmin):
-       list_display = ('original', 'user')
+    list_display = ('pk', 'original', 'user')
+
 
 admin.site.register(Image, ImageModelAdmin)
--- homepage/blogs/models.py
+++ homepage/blogs/models.py
@@ -1,11 +1,18 @@
+import re
+
+from io import BytesIO
+
 from django.db import models
 from django.core.urlresolvers import reverse
+from django.core.files.images import ImageFile
 
 from ckeditor_uploader.fields import RichTextUploadingField
 
 from model_utils.models import TimeStampedModel
 
 from slugify import slugify
+
+from PIL import Image as PILImage
 
 from ..users.models import User
 
@@ -42,10 +49,109 @@ class BlogPost(TimeStampedModel):
     def get_slug(self):
         return slugify(self.title)
 
+    @property
+    def processed_content(self):
+        processed = self.content
+        images = re.findall('img_\d+', processed)
+        for image in images:
+            pk = int(image.split('_')[-1])
+            img = Image.objects.get(pk=pk)
+            processed = processed.replace(image, img.get_img_tag())
+        return processed
+
 
 class Image(TimeStampedModel):
     user = models.ForeignKey(User)
-    original = models.ImageField(upload_to='blogs_images')
+    original = models.ImageField(
+        upload_to='blogs_images/originals',
+        height_field='original_height',
+        width_field='original_width')
+    original_height = models.PositiveIntegerField(blank=True, null=True)
+    original_width= models.PositiveIntegerField(blank=True, null=True)
+
+    img_xl = models.ImageField(
+        upload_to='blogs_images/resized',
+        height_field='xl_height',
+        width_field='xl_width',
+        null=True,
+        blank=True,)
+    xl_height = models.PositiveIntegerField(blank=True, null=True)
+    xl_width = models.PositiveIntegerField(blank=True, null=True)
+
+    img_lg = models.ImageField(
+        upload_to='blogs_images/resized',
+        height_field='lg_height',
+        width_field='lg_width',
+        null=True,
+        blank=True,)
+    lg_height = models.PositiveIntegerField(blank=True, null=True)
+    lg_width = models.PositiveIntegerField(blank=True, null=True)
+
+    img_md = models.ImageField(
+        upload_to='blogs_images/resized',
+        height_field='md_height',
+        width_field='md_width',
+        null=True,
+        blank=True,)
+    md_height = models.PositiveIntegerField(blank=True, null=True)
+    md_width = models.PositiveIntegerField(blank=True, null=True)
+
+    img_sm = models.ImageField(
+        upload_to='blogs_images/resized',
+        height_field='sm_height',
+        width_field='sm_width',
+        null=True,
+        blank=True,)
+    sm_height = models.PositiveIntegerField(blank=True, null=True)
+    sm_width = models.PositiveIntegerField(blank=True, null=True)
+
+    img_xs = models.ImageField(
+        upload_to='blogs_images/resized',
+        height_field='xs_height',
+        width_field='xs_width',
+        null=True,
+        blank=True,)
+    xs_height = models.PositiveIntegerField(blank=True, null=True)
+    xs_width = models.PositiveIntegerField(blank=True, null=True)
+
+    sizes = [
+        (2200, 'img_xl'),
+        (1100, 'img_lg'),
+        (768, 'img_md'),
+        (500, 'img_sm'),
+        (300, 'img_xs'),
+    ]
+
+    def create_resized_images(self):
+        for size, attr_name in self.sizes:
+            img_size = size, size
+            im = PILImage.open(self.original)
+            im.thumbnail(img_size)
+            im_io = BytesIO()
+            im.save(im_io, format='JPEG')
+            original_name = (self.original.name
+                             .split('/')[-1]
+                             .replace('.JPG', ''))
+            resized_name = '{}_resized_{}.JPG'.format(original_name, img_size[0])
+            img_attr = getattr(self, attr_name)
+            img_attr.save(resized_name, im_io, save=False)
 
     def __str__(self):
-        return 'foo'
+        return self.original.name
+
+    def save(self, *args, **kwargs):
+        self.create_resized_images()
+        return super().save(*args, **kwargs)
+
+    def get_srcset(self):
+        sources = []
+        for size, attr_name in self.sizes:
+            img = getattr(self, attr_name)
+            sources.append(img.url)
+        return ' '.join(sources)
+
+    def get_img_tag(self):
+        return (
+            '<img width="100%" srcset="{srcset}" src="{src}"'
+            '</img>'
+        ).format(srcset=self.get_srcset(), src=self.img_xl.url)

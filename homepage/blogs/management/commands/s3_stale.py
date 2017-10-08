e735a703d1cf94502a219d837f657046c30c1d43
Jochen Wersdörfer <jochen@wersdoerfer.de>
2017-10-08 17:59:28 +0200

    show/delete stale media files

---
+++ homepage/blogs/management/commands/s3_stale.py
@@ -0,0 +1,71 @@
+import os
+
+from django.core.management.base import BaseCommand
+from django.core.files.storage import default_storage
+from django.core.files.storage import get_storage_class
+
+from ...models import BlogImage
+from ...models import BlogVideo
+
+from ...utils import storage_walk_paths
+
+class Command(BaseCommand):
+    help = (
+        'show media files which are in the filesystem (s3, locale), '
+        'but not in database and optionally delete them')
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--delete',
+            action='store_true',
+            dest='delete',
+            default=False,
+            help='Delete stale files instead of only showing them',
+        )
+
+    def get_paths(self, storage):
+        paths = {}
+        for path in storage_walk_paths(storage):
+            size = storage.size(path)
+            paths[path] = size
+            # print(path, size / 2 ** 20)
+        return paths
+
+    def get_models_paths(self):
+        image_fields = ['original', 'img_xl', 'img_lg', 'img_md', 'img_sm', 'img_xs']
+        paths = set()
+        for image in BlogImage.objects.all():
+            for field in image_fields:
+                paths.add(getattr(image, field))
+        for video in BlogVideo.objects.all():
+            paths.add(video.original.name)
+        return paths
+
+    def handle(self, *args, **options):
+        print(args, options)
+        paths_from_models = self.get_models_paths()
+
+        print('stale s3')
+        s3 = get_storage_class('storages.backends.s3boto3.S3Boto3Storage')()
+        s3_paths = self.get_paths(s3)
+        stale_s3 = {}
+        for path, size in s3_paths.items():
+            if path not in paths_from_models:
+                print(path)
+                stale_s3[path] = size
+        print('stale s3 size: {} Mb'.format(sum(stale_s3.values()) / 2 ** 20))
+
+        print('stale locale')
+        locale_paths = self.get_paths(default_storage)
+        stale_locale = {}
+        for path, size in locale_paths.items():
+            if path not in paths_from_models:
+                print(path)
+                stale_locale[path] = size
+        print('stale locale size: {} Mb'.format(sum(stale_locale.values()) / 2 ** 20))
+
+        if options['delete']:
+            for path in stale_s3.keys():
+                s3.delete(path)
+            for path in stale_locale.keys():
+                default_storage.delete(path)

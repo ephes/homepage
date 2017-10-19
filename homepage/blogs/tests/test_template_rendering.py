54bd2ce5a56e25bd18ccfb118bcc95fcfe14b98b
Jochen Wersdörfer <jochen@wersdoerfer.de>
2017-10-19 20:41:24 +0200

    test for new media in context method

--- homepage/blogs/tests/test_template_rendering.py
+++ homepage/blogs/tests/test_template_rendering.py
@@ -11,8 +11,16 @@ def test_template_render(test_templ):
 
 
 @pytest.mark.django_db()
-def test_blogimage_render(img_templ, blog_image):
+def test_blogimage_render_without_context(img_templ, blog_image):
     template = Template(img_templ)
     context = Context({})
+    with pytest.raises(KeyError) as excinfo:
+        result = template.render(context)
+
+
+@pytest.mark.django_db()
+def test_blogimage_render_with_context(img_templ, blog_image):
+    template = Template(img_templ)
+    context = Context({'image': {blog_image.pk: blog_image}})
     result = template.render(context)
     assert '<a href' in result

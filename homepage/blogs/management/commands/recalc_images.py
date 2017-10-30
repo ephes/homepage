6630287507a4fde474d907fa5997e6273736bd79
Jochen Wersdörfer <jochen@wersdoerfer.de>
2017-10-30 15:17:29 +0100

    removed obsolete method + docstring

--- homepage/blogs/management/commands/recalc_images.py
+++ homepage/blogs/management/commands/recalc_images.py
@@ -5,17 +5,7 @@ from ...models import BlogImage
 
 class Command(BaseCommand):
     help = (
-        'show media files which are in the filesystem (s3, locale), '
-        'but not in database and optionally delete them')
-
-    def add_arguments(self, parser):
-        parser.add_argument(
-            '--delete',
-            action='store_true',
-            dest='delete',
-            default=False,
-            help='Delete stale files instead of only showing them',
-        )
+        'recalc resized images')
 
     def handle(self, *args, **options):
         for image in BlogImage.objects.all():

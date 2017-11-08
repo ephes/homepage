83b9611843a3e7971b2cb48d143a6f8f88942883
Jochen Wersdörfer <jochen@wersdoerfer.de>
2017-11-08 17:02:36 +0100

    forgot one testcase for unpublished entries

--- homepage/blogs/tests/test_blogpost_detail.py
+++ homepage/blogs/tests/test_blogpost_detail.py
@@ -21,10 +21,22 @@ class TestBlogpostDetail:
         slugs = {'blog_slug': blogpost.blog.slug, 'slug': blogpost.slug}
         detail_url = reverse('blogs:blogpost_detail', kwargs=slugs)
 
-        print(blogpost.published)
-
         r = client.get(detail_url)
         assert r.status_code == 404
 
         content = r.content.decode('utf-8')
         assert blogpost.title not in content
+
+    def test_get_blogpost_detail_not_published_authenticated(self, client, unpublished_blogpost):
+        blogpost = unpublished_blogpost
+        slugs = {'blog_slug': blogpost.blog.slug, 'slug': blogpost.slug}
+        detail_url = reverse('blogs:blogpost_detail', kwargs=slugs)
+
+        r = client.login(username=blogpost.author.username, password="password")
+
+        r = client.get(detail_url)
+        assert r.status_code == 200
+
+        content = r.content.decode('utf-8')
+        assert 'html' in content
+        assert blogpost.title in content

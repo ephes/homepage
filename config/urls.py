9c4518f6c6b4913c6781c309ab7d36a9885cf98b
Jochen Wersdörfer <jochen@wersdoerfer.de>
2022-11-05 22:57:19 +0100

    #7 fixed urlpatterns

--- config/urls.py
+++ config/urls.py
@@ -1,77 +1,90 @@
 from django.conf import settings
-from django.urls import include, path, re_path
-from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.views.generic import TemplateView
-from django.views import defaults as default_views
 from django.http import HttpResponse
-
+from django.urls import include, path, re_path
+from django.views import defaults as default_views
+from django.views.generic import TemplateView
+from rest_framework.authtoken import views as authtokenviews
+from rest_framework.documentation import include_docs_urls
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
-from rest_framework.documentation import include_docs_urls
-from rest_framework.authtoken import views as authtokenviews
-
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
-    url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
-    url(r'^jochen/$', TemplateView.as_view(template_name='pages/jochen.html'), name='jochen'),
-    url(r'^katharina/$', TemplateView.as_view(template_name='pages/katharina.html'), name='katharina'),
-
+    path("home/", TemplateView.as_view(template_name="pages/home.html"), name="home"),
+    path(
+        "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
+    ),
+    path(
+        "jochen/",
+        TemplateView.as_view(template_name="pages/jochen.html"),
+        name="jochen",
+    ),
+    path(
+        "katharina/",
+        TemplateView.as_view(template_name="pages/katharina.html"),
+        name="katharina",
+    ),
     # Django Admin, use {% url 'admin:index' %}
-    url(settings.ADMIN_URL, admin.site.urls),
-
+    path(settings.ADMIN_URL, admin.site.urls),
     # User management
-    url(r'^users/', include('homepage.users.urls', namespace='users')),
-    url(r'^accounts/', include('allauth.urls')),
-
+    path("users/", include("homepage.users.urls", namespace="users")),
+    path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
-
     # Cast Blog
-    url(r'^blogs/', include('cast.urls', namespace='cast')),
-
+    path("blogs/", include("cast.urls", namespace="cast")),
     # Threadedcomments
-    re_path(r'^show/comments/', include('fluent_comments.urls')),
-
+    re_path(r"^show/comments/", include("fluent_comments.urls")),
     # Fulltext Search
-    url(r"^search/", include("watson.urls", namespace="watson")),
-
+    path("search/", include("watson.urls", namespace="watson")),
     # Indieweb
-    url(r"^indieweb/", include("indieweb.urls")),
-
+    re_path(r"^indieweb/", include("indieweb.urls")),
     # rest
-    url(r'^api/api-token-auth/', authtokenviews.obtain_auth_token),
+    re_path(r"^api/api-token-auth/", authtokenviews.obtain_auth_token),
     # url(r'api/', include('homepage.blogs.api.urls', namespace='api')),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^docs/', include_docs_urls(title='My Blog API service')),
+    re_path(r"^api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    re_path(r"^docs/", include_docs_urls(title="My Blog API service")),
     # Uploads
     path("uploads/", include("filepond.urls", namespace="filepond")),
-
     # Wagtail
     path(settings.WAGTAILADMIN_BASE_URL, include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
     path("", include(wagtail_urls)),  # default is wagtail
-
     # robots.txt
-    url(r'^robots.txt',
-        lambda x: HttpResponse("User-Agent: *\nDisallow: /*claas", content_type="text/plain"),
-        name="robots_file"),
-
+    re_path(
+        r"^robots.txt",
+        lambda x: HttpResponse(
+            "User-Agent: *\nDisallow: /*claas", content_type="text/plain"
+        ),
+        name="robots_file",
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     # This allows the error pages to be debugged during development, just visit
     # these url in browser to see how these error pages look like.
     urlpatterns += [
-        url(r'^400/$', default_views.bad_request, kwargs={'exception': Exception('Bad Request!')}),
-        url(r'^403/$', default_views.permission_denied, kwargs={'exception': Exception('Permission Denied')}),
-        url(r'^404/$', default_views.page_not_found, kwargs={'exception': Exception('Page not Found')}),
-        url(r'^500/$', default_views.server_error),
+        path(
+            "400/",
+            default_views.bad_request,
+            kwargs={"exception": Exception("Bad Request!")},
+        ),
+        path(
+            "403/",
+            default_views.permission_denied,
+            kwargs={"exception": Exception("Permission Denied")},
+        ),
+        path(
+            "404/",
+            default_views.page_not_found,
+            kwargs={"exception": Exception("Page not Found")},
+        ),
+        path("500/", default_views.server_error),
     ]
-    if 'debug_toolbar' in settings.INSTALLED_APPS:
+    if "debug_toolbar" in settings.INSTALLED_APPS:
         import debug_toolbar
+
         urlpatterns = [
-            url(r'^__debug__/', include(debug_toolbar.urls)),
+            path("__debug__/", include(debug_toolbar.urls)),
         ] + urlpatterns
--- homepage/users/urls.py
+++ homepage/users/urls.py
@@ -1,15 +1,15 @@
-from django.conf.urls import url
+from django.urls import path, re_path
 
 from . import views
 
 app_name = "users"
 urlpatterns = [
-    url(regex=r"^$", view=views.UserListView.as_view(), name="list"),
-    url(regex=r"^~redirect/$", view=views.UserRedirectView.as_view(), name="redirect"),
-    url(
-        regex=r"^(?P<username>[\w.@+-]+)/$",
+    path("", view=views.UserListView.as_view(), name="list"),
+    re_path(r"^~redirect/$", view=views.UserRedirectView.as_view(), name="redirect"),
+    re_path(
+        r"^(?P<username>[\w.@+-]+)/$",
         view=views.UserDetailView.as_view(),
         name="detail",
     ),
-    url(regex=r"^~update/$", view=views.UserUpdateView.as_view(), name="update"),
+    re_path(r"^~update/$", view=views.UserUpdateView.as_view(), name="update"),
 ]

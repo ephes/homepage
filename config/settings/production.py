067f58dd313476fcd6850fc7a74539780391dc16
Jochen Wersdörfer <jochen@wersdoerfer.de>
2022-11-06 01:50:29 +0100

    #7 had to deactivate redis

--- config/settings/production.py
+++ config/settings/production.py
@@ -10,11 +10,9 @@ Production Configurations
 
 
 """
-import os
 import logging
-
+import os
 from tempfile import SpooledTemporaryFile
-
 
 from .base import *  # noqa
 
@@ -22,21 +20,25 @@ from .base import *  # noqa
 # ------------------------------------------------------------------------------
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 # Raises ImproperlyConfigured exception if DJANGO_SECRET_KEY not in os.environ
-SECRET_KEY = env('DJANGO_SECRET_KEY')
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 
 # This ensures that Django will be able to detect a secure connection
 # properly on Heroku.
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 # raven sentry client
 # See https://docs.sentry.io/clients/python/integrations/django/
-INSTALLED_APPS += ['raven.contrib.django.raven_compat', ]
+INSTALLED_APPS += [
+    "raven.contrib.django.raven_compat",
+]
 
 # Use Whitenoise to serve static files
 # See: https://whitenoise.readthedocs.io/
-WHITENOISE_MIDDLEWARE = ['whitenoise.middleware.WhiteNoiseMiddleware', ]
+WHITENOISE_MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+]
 MIDDLEWARE = WHITENOISE_MIDDLEWARE + MIDDLEWARE
-RAVEN_MIDDLEWARE = ['raven.contrib.django.raven_compat.middleware.SentryResponseErrorIdMiddleware']
+RAVEN_MIDDLEWARE = ["raven.contrib.django.raven_compat.middleware.SentryResponseErrorIdMiddleware"]
 MIDDLEWARE = RAVEN_MIDDLEWARE + MIDDLEWARE
 
 
@@ -47,27 +49,32 @@ MIDDLEWARE = RAVEN_MIDDLEWARE + MIDDLEWARE
 
 # set this to 60 seconds and then to 518400 when you can prove it works
 SECURE_HSTS_SECONDS = 60
-SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool(
-    'DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS', default=True)
-SECURE_CONTENT_TYPE_NOSNIFF = env.bool(
-    'DJANGO_SECURE_CONTENT_TYPE_NOSNIFF', default=True)
+SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool("DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS", default=True)
+SECURE_CONTENT_TYPE_NOSNIFF = env.bool("DJANGO_SECURE_CONTENT_TYPE_NOSNIFF", default=True)
 SECURE_BROWSER_XSS_FILTER = True
 SESSION_COOKIE_SECURE = True
 SESSION_COOKIE_HTTPONLY = True
-SECURE_SSL_REDIRECT = env.bool('DJANGO_SECURE_SSL_REDIRECT', default=True)
+SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
 CSRF_COOKIE_SECURE = True
 CSRF_COOKIE_HTTPONLY = True
-X_FRAME_OPTIONS = 'DENY'
+X_FRAME_OPTIONS = "DENY"
 SECURE_HSTS_PRELOAD = True
 
 # SITE CONFIGURATION
 # ------------------------------------------------------------------------------
 # Hosts/domain names that are valid for this site
 # See https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['wersdoerfer.de', ])
+ALLOWED_HOSTS = env.list(
+    "DJANGO_ALLOWED_HOSTS",
+    default=[
+        "wersdoerfer.de",
+    ],
+)
 # END SITE CONFIGURATION
 
-INSTALLED_APPS += ['gunicorn', ]
+INSTALLED_APPS += [
+    "gunicorn",
+]
 
 
 # STORAGE CONFIGURATION
@@ -75,17 +82,19 @@ INSTALLED_APPS += ['gunicorn', ]
 # Uploaded Media Files
 # ------------------------
 # See: http://django-storages.readthedocs.io/en/latest/index.html
-INSTALLED_APPS += ['storages', ]
+INSTALLED_APPS += [
+    "storages",
+]
 
-AWS_ACCESS_KEY_ID = env('DJANGO_AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = env('DJANGO_AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = env('DJANGO_AWS_STORAGE_BUCKET_NAME')
+AWS_ACCESS_KEY_ID = env("DJANGO_AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = env("DJANGO_AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = env("DJANGO_AWS_STORAGE_BUCKET_NAME")
 AWS_AUTO_CREATE_BUCKET = True
 AWS_QUERYSTRING_AUTH = False
-AWS_S3_REGION_NAME = 'eu-central-1'
-AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_REGION_NAME = "eu-central-1"
+AWS_S3_SIGNATURE_VERSION = "s3v4"
 AWS_S3_FILE_OVERWRITE = True
-AWS_S3_CUSTOM_DOMAIN = env('CLOUDFRONT_DOMAIN')
+AWS_S3_CUSTOM_DOMAIN = env("CLOUDFRONT_DOMAIN")
 
 # AWS cache settings, don't change unless you know what you're doing:
 AWS_EXPIRY = 60 * 60 * 24 * 7
@@ -93,85 +102,84 @@ AWS_EXPIRY = 60 * 60 * 24 * 7
 # TODO See: https://github.com/jschneier/django-storages/issues/47
 # Revert the following and use str after the above-mentioned bug is fixed in
 # either django-storage-redux or boto
-control = 'max-age=%d, s-maxage=%d, must-revalidate' % (AWS_EXPIRY, AWS_EXPIRY)
-AWS_HEADERS = {
-    'Cache-Control': bytes(control, encoding='latin-1')
-}
+control = "max-age=%d, s-maxage=%d, must-revalidate" % (AWS_EXPIRY, AWS_EXPIRY)
+AWS_HEADERS = {"Cache-Control": bytes(control, encoding="latin-1")}
 
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class CustomS3Boto3Storage(S3Boto3Storage):
+    """
+    This is our custom version of S3Boto3Storage that fixes a bug in
+    boto3 where the passed in file is closed upon upload.
+
+    https://github.com/boto/boto3/issues/929
+    https://github.com/matthewwithanm/django-imagekit/issues/391
+    """
+
+    file_overwrite = False
+    default_acl = "public-read"
+
+    def _save_content(self, obj, content, parameters):
         """
-        This is our custom version of S3Boto3Storage that fixes a bug in
-        boto3 where the passed in file is closed upon upload.
-
-        https://github.com/boto/boto3/issues/929
-        https://github.com/matthewwithanm/django-imagekit/issues/391
+        We create a clone of the content file as when this is passed to boto3
+        it wrongly closes the file upon upload where as the storage backend
+        expects it to still be open
         """
+        # Seek our content back to the start
+        content.seek(0, os.SEEK_SET)
 
-        file_overwrite = False
-        default_acl = "public-read"
+        # Create a temporary file that will write to disk after a specified size
+        content_autoclose = SpooledTemporaryFile()
 
-        def _save_content(self, obj, content, parameters):
-                """
-                We create a clone of the content file as when this is passed to boto3
-                it wrongly closes the file upon upload where as the storage backend
-                expects it to still be open
-                """
-                # Seek our content back to the start
-                content.seek(0, os.SEEK_SET)
+        # Write our original content into our copy that will be closed by boto3
+        content_autoclose.write(content.read())
 
-                # Create a temporary file that will write to disk after a specified size
-                content_autoclose = SpooledTemporaryFile()
+        # Upload the object which will auto close the content_autoclose instance
+        super()._save_content(obj, content_autoclose, parameters)
 
-                # Write our original content into our copy that will be closed by boto3
-                content_autoclose.write(content.read())
-
-                # Upload the object which will auto close the content_autoclose instance
-                super(CustomS3Boto3Storage, self)._save_content(
-                        obj, content_autoclose, parameters
-                )
-
-                # Cleanup if this is fixed upstream our duplicate should always close
-                if not content_autoclose.closed:
-                        content_autoclose.close()
+        # Cleanup if this is fixed upstream our duplicate should always close
+        if not content_autoclose.closed:
+            content_autoclose.close()
 
 
 # URL that handles the media served from MEDIA_ROOT, used for managing
 # stored files.
-MEDIA_URL = 'https://s3.amazonaws.com/%s/' % AWS_STORAGE_BUCKET_NAME
+MEDIA_URL = "https://s3.amazonaws.com/%s/" % AWS_STORAGE_BUCKET_NAME
 # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-DEFAULT_FILE_STORAGE = 'config.settings.production.CustomS3Boto3Storage'
+DEFAULT_FILE_STORAGE = "config.settings.production.CustomS3Boto3Storage"
 
 
 # Static Assets
 # ------------------------
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # EMAIL
 # ------------------------------------------------------------------------------
-DEFAULT_FROM_EMAIL = env('DJANGO_DEFAULT_FROM_EMAIL',
-                         default='Homepage <jochen-homepage@wersdoerfer.de>')
-EMAIL_SUBJECT_PREFIX = env('DJANGO_EMAIL_SUBJECT_PREFIX', default='[Homepage]')
-SERVER_EMAIL = env('DJANGO_SERVER_EMAIL', default=DEFAULT_FROM_EMAIL)
+DEFAULT_FROM_EMAIL = env("DJANGO_DEFAULT_FROM_EMAIL", default="Homepage <jochen-homepage@wersdoerfer.de>")
+EMAIL_SUBJECT_PREFIX = env("DJANGO_EMAIL_SUBJECT_PREFIX", default="[Homepage]")
+SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
 
 # Anymail with Mailgun
-INSTALLED_APPS += ['anymail', ]
-ANYMAIL = {
-    'MAILGUN_API_KEY': env('DJANGO_MAILGUN_API_KEY'),
-    'MAILGUN_SENDER_DOMAIN': env('MAILGUN_SENDER_DOMAIN')
-}
-EMAIL_BACKEND = 'anymail.backends.mailgun.MailgunBackend'
+INSTALLED_APPS += [
+    "anymail",
+]
+ANYMAIL = {"MAILGUN_API_KEY": env("DJANGO_MAILGUN_API_KEY"), "MAILGUN_SENDER_DOMAIN": env("MAILGUN_SENDER_DOMAIN")}
+EMAIL_BACKEND = "anymail.backends.mailgun.MailgunBackend"
 
 # TEMPLATE CONFIGURATION
 # ------------------------------------------------------------------------------
 # See:
 # https://docs.djangoproject.com/en/dev/ref/templates/api/#django.template.loaders.cached.Loader
-TEMPLATES[0]['OPTIONS']['loaders'] = [
-    ('django.template.loaders.cached.Loader', [
-        'django.template.loaders.filesystem.Loader', 'django.template.loaders.app_directories.Loader', ]),
+TEMPLATES[0]["OPTIONS"]["loaders"] = [
+    (
+        "django.template.loaders.cached.Loader",
+        [
+            "django.template.loaders.filesystem.Loader",
+            "django.template.loaders.app_directories.Loader",
+        ],
+    ),
 ]
 
 # DATABASE CONFIGURATION
@@ -179,94 +187,99 @@ TEMPLATES[0]['OPTIONS']['loaders'] = [
 
 # Use the Heroku-style specification
 # Raises ImproperlyConfigured exception if DATABASE_URL not in os.environ
-DATABASES['default'] = env.db('DATABASE_URL')
+DATABASES["default"] = env.db("DATABASE_URL")
 
 # CACHING
 # ------------------------------------------------------------------------------
 
-REDIS_LOCATION = '{0}/{1}'.format(env('REDIS_URL', default='redis://127.0.0.1:6379'), 0)
-# Heroku URL does not pass the DB number, so we parse it in
-CACHES = {
-    'default': {
-        'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': REDIS_LOCATION,
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-            'IGNORE_EXCEPTIONS': True,  # mimics memcache behavior.
-                                        # http://niwinz.github.io/django-redis/latest/#_memcached_exceptions_behavior
-        }
-    }
-}
+# REDIS_LOCATION = '{0}/{1}'.format(env('REDIS_URL', default='redis://127.0.0.1:6379'), 0)
+## Heroku URL does not pass the DB number, so we parse it in
+# CACHES = {
+#    'default': {
+#        'BACKEND': 'django_redis.cache.RedisCache',
+#        'LOCATION': REDIS_LOCATION,
+#        'OPTIONS': {
+#            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+#            'IGNORE_EXCEPTIONS': True,  # mimics memcache behavior.
+#                                        # http://niwinz.github.io/django-redis/latest/#_memcached_exceptions_behavior
+#        }
+#    }
+# }
 
 
 # Sentry Configuration
-SENTRY_DSN = env('DJANGO_SENTRY_DSN')
-SENTRY_CLIENT = env('DJANGO_SENTRY_CLIENT', default='raven.contrib.django.raven_compat.DjangoClient')
+SENTRY_DSN = env("DJANGO_SENTRY_DSN")
+SENTRY_CLIENT = env("DJANGO_SENTRY_CLIENT", default="raven.contrib.django.raven_compat.DjangoClient")
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-    'root': {
-        'level': 'WARNING',
-        'handlers': ['sentry', ],
+    "version": 1,
+    "disable_existing_loggers": True,
+    "root": {
+        "level": "WARNING",
+        "handlers": [
+            "sentry",
+        ],
     },
-    'formatters': {
-        'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s '
-                      '%(process)d %(thread)d %(message)s'
-        },
+    "formatters": {
+        "verbose": {"format": "%(levelname)s %(asctime)s %(module)s " "%(process)d %(thread)d %(message)s"},
     },
-    'handlers': {
-        'sentry': {
-            'level': 'ERROR',
-            'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
+    "handlers": {
+        "sentry": {
+            "level": "ERROR",
+            "class": "raven.contrib.django.raven_compat.handlers.SentryHandler",
         },
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose'
-        }
+        "console": {"level": "DEBUG", "class": "logging.StreamHandler", "formatter": "verbose"},
     },
-    'loggers': {
-        'django.db.backends': {
-            'level': 'ERROR',
-            'handlers': ['console', ],
-            'propagate': False,
+    "loggers": {
+        "django.db.backends": {
+            "level": "ERROR",
+            "handlers": [
+                "console",
+            ],
+            "propagate": False,
         },
-        'raven': {
-            'level': 'DEBUG',
-            'handlers': ['console', ],
-            'propagate': False,
+        "raven": {
+            "level": "DEBUG",
+            "handlers": [
+                "console",
+            ],
+            "propagate": False,
         },
-        'sentry.errors': {
-            'level': 'DEBUG',
-            'handlers': ['console', ],
-            'propagate': False,
+        "sentry.errors": {
+            "level": "DEBUG",
+            "handlers": [
+                "console",
+            ],
+            "propagate": False,
         },
-        'django.security.DisallowedHost': {
-            'level': 'ERROR',
-            'handlers': ['console', 'sentry', ],
-            'propagate': False,
+        "django.security.DisallowedHost": {
+            "level": "ERROR",
+            "handlers": [
+                "console",
+                "sentry",
+            ],
+            "propagate": False,
         },
-        'cast': {
-            'handlers': ['console',],
-            'propagate': True,
-            'level': 'DEBUG',
+        "cast": {
+            "handlers": [
+                "console",
+            ],
+            "propagate": True,
+            "level": "DEBUG",
         },
-        'indieweb': {
-            'handlers': ['console',],
-            'propagate': True,
-            'level': 'DEBUG',
+        "indieweb": {
+            "handlers": [
+                "console",
+            ],
+            "propagate": True,
+            "level": "DEBUG",
         },
     },
 }
-SENTRY_CELERY_LOGLEVEL = env.int('DJANGO_SENTRY_LOG_LEVEL', logging.INFO)
-RAVEN_CONFIG = {
-    'CELERY_LOGLEVEL': env.int('DJANGO_SENTRY_LOG_LEVEL', logging.INFO),
-    'DSN': SENTRY_DSN
-}
+SENTRY_CELERY_LOGLEVEL = env.int("DJANGO_SENTRY_LOG_LEVEL", logging.INFO)
+RAVEN_CONFIG = {"CELERY_LOGLEVEL": env.int("DJANGO_SENTRY_LOG_LEVEL", logging.INFO), "DSN": SENTRY_DSN}
 
 # Custom Admin URL, use {% url 'admin:index' %}
-ADMIN_URL = env('DJANGO_ADMIN_URL')
+ADMIN_URL = env("DJANGO_ADMIN_URL")
 
 # Your production stuff: Below this line define 3rd party library settings
 # ------------------------------------------------------------------------------

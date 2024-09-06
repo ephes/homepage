975b438246b6ace2aeee92afd571143f0ad331a8
Jochen Wersdörfer <jochen@wersdoerfer.de>
2024-09-06 17:32:13 +0200

    Add the token link to the list display to make it easier to test / copy it..

--- homepage/resume/admin.py
+++ homepage/resume/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 
 from .models import CVToken
 
@@ -6,5 +8,11 @@ from .models import CVToken
 @admin.register(CVToken)
 class CVTokenAdmin(admin.ModelAdmin):
     fields = ("receiver",)
-    list_display = ("token", "created", "receiver")
+    list_display = ("token", "created", "receiver", "token_link")
     search_fields = ("receiver",)
+
+    @admin.display(description="Token Link")
+    def token_link(self, obj):
+        cv_url = reverse("resume:cv")
+        cv_url_with_token = f"{cv_url}?token={obj.token}"
+        return format_html('<a href="{}" target="_blank">{}</a>', cv_url_with_token, cv_url_with_token)
